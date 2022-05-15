3ea461efdf0b31b0fbdd94cda82b5cb1f1bd637b
BeGeos <montepeloso.m@gmail.com>
2022-05-15 15:50:30 +0200

    my first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const showContainers = document.querySelectorAll(".show-replies");
+
+showContainers.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    let parentContainer = e.target.closest(".comment__container");
+    let _id = parentContainer.id;
+    if (_id) {
+      let childrenContainer = parentContainer.querySelectorAll(
+        `[dataset=${_id}]`
+      );
+      childrenContainer.forEach((child) => child.classList.toggle("opened"));
+    }
+  })
+);
